2d8c355f5792ff40c9f69a0a3f5727ebc304b1c9
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-07-24 07:30:19 -0300

    Change lookup users

--- src/main/java/com/example/config/AppRunner.java
+++ src/main/java/com/example/config/AppRunner.java
@@ -26,9 +26,9 @@ public class AppRunner implements CommandLineRunner {
         long start = System.currentTimeMillis();
 
         // Kick of multiple, asynchronous lookups
-        CompletableFuture<User> page1 = gitHubLookupService.findUser("Edufreitass");
-        CompletableFuture<User> page2 = gitHubLookupService.findUser("L7AN");
-        CompletableFuture<User> page3 = gitHubLookupService.findUser("acenelio");
+        CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
+        CompletableFuture<User> page2 = gitHubLookupService.findUser("CloudFoundry");
+        CompletableFuture<User> page3 = gitHubLookupService.findUser("Spring-Projects");
 
         // Wait until they are all done
         CompletableFuture.allOf(page1, page2, page3).join();
