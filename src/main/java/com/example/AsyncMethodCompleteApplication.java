d7ae0408de364c39fb7c3ce072f9a8a291ed55e3
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-07-24 07:15:59 -0300

    Create Executor

--- src/main/java/com/example/AsyncMethodCompleteApplication.java
+++ src/main/java/com/example/AsyncMethodCompleteApplication.java
@@ -2,12 +2,29 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class AsyncMethodCompleteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsyncMethodCompleteApplication.class, args);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
 	}
 
 }
